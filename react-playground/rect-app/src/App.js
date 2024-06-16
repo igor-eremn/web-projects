c7d96574404ddb224001bbceab248e9696b4467d
Igor Eremin <77459655+success-412@users.noreply.github.com>
2024-06-15 18:04:25 -0700

    useRef

--- react-playground/rect-app/src/App.js
+++ react-playground/rect-app/src/App.js
@@ -1,21 +1,32 @@
 import './App.css';
-import { useReducer } from "react";
+import { useRef } from "react";
 
 function App() {
-  const [checked, setChecked] = 
-    useReducer(checked => !checked , false);
+  const txtTitle = useRef();
+  const hexColor = useRef();
+
+  const submit = (e) => {
+    e.preventDefault();
+    const title = txtTitle.current.value;
+    const hex = hexColor.current.value;
+    alert(`${title}, ${hex}`);
+    txtTitle.current.value = "";
+    hexColor.current.value = "";
+  }
 
   return (
-    <div className="App">
+    <form onSubmit={submit}>
       <input 
-        type="checkbox" 
-        value={checked} 
-        onChange={setChecked}
+        ref={txtTitle}
+        type="text" 
+        placeholder="color title..." 
       />
-      <label>
-        {checked ? "Checked" : "Not Checked"}
-      </label>
-    </div>
+      <input 
+        ref={hexColor}
+        type="color"
+      />
+      <button>Add</button>
+    </form>
   );
 }
 
