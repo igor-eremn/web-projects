d9e3f91fbaeefa0f48cb78f6fa81e5715e60e5a2
Igor Eremin <77459655+success-412@users.noreply.github.com>
2024-06-15 17:52:45 -0700

    useReducer

--- react-playground/rect-app/src/App.js
+++ react-playground/rect-app/src/App.js
@@ -1,47 +1,20 @@
 import './App.css';
-import { useState, useEffect } from "react";
+import { useReducer } from "react";
 
 function App() {
-  const [state, setState] = useState("State 0");
-  const [check, setCheck] = useState(0);
-
-  const [time, setTime] = useState("Morning");
-  const [timeCheck, setTimeCheck] = useState(0);
-
-  useEffect(() => {
-    console.log(`Current state: ${state}`);
-  }, [state]);
-
-  useEffect(()=>{
-    console.log(`Current Time: ${time}`);
-  }, [time]);
+  const [checked, setChecked] = 
+    useReducer(checked => !checked , false);
 
   return (
     <div className="App">
-      <h1>Current State is { state }</h1>
-      <button onClick={() => {
-        if(check === 0){
-          setCheck(1);
-          setState("State 1");
-        }else{
-          setCheck(0);
-          setState("State 0");
-        }
-      }}>Change State</button>
-
-      <h2>Current Time is {time}</h2>
-      <button onClick={() => {
-        if(timeCheck === 0){
-          setTimeCheck(1);
-          setTime("Afternoon");
-        }else if(timeCheck === 1){
-          setTimeCheck(2);
-          setTime("Evening");
-        }else{
-          setTimeCheck(0);
-          setTime("Morning");
-        }
-      }}>Change Time</button>
+      <input 
+        type="checkbox" 
+        value={checked} 
+        onChange={setChecked}
+      />
+      <label>
+        {checked ? "Checked" : "Not Checked"}
+      </label>
     </div>
   );
 }
