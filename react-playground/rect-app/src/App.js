7262a6d38b3dd672fdae045d3826e6bf0c6bb2c0
Igor Eremin <77459655+success-412@users.noreply.github.com>
2024-06-16 20:11:20 -0700

    added some pictures to buttons

--- react-playground/rect-app/src/App.js
+++ react-playground/rect-app/src/App.js
@@ -60,10 +60,38 @@ export function Counter(){
       <CreateNav />
       <h1>Counter</h1>
       <p>Count: {count}</p>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={saveCount}>Save & Reset</button>
-      <button onClick={clearStorage}>Clear Storage</button>
+      <button>
+        <img 
+          src="https://www.freeiconspng.com/thumbs/plus-icon/plus-icon-black-2.png" 
+          alt="Add Conuter" 
+          onClick={increment}
+          height="50"
+        />
+      </button>
+      <button>
+        <img 
+          src="https://pngimg.com/d/minus_PNG6.png" 
+          alt="Minus Conuter" 
+          onClick={decrement}
+          height="50"
+        />
+      </button>
+      <button>
+        <img 
+          src="https://images.pngnice.com/download/2007/Save-Button-PNG-Transparent-Photo.png" 
+          alt="Save Conuter" 
+          onClick={saveCount}
+          height="50"
+        />
+      </button>
+      <button>
+        <img 
+          src="https://cdn1.iconfinder.com/data/icons/essential-ui-lineal-set/512/bin-512.png" 
+          alt="Clear Storage" 
+          onClick={clearStorage}
+          height="50"
+        />
+      </button>
       <ul>
         {savedCounts.map((savedCount, index) => (
           <li key={index}>Saved Count: {savedCount.count}; When: {savedCount.time}</li>
